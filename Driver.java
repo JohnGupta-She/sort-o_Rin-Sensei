0f8b42d8ae77964248aac6a0073135b95cb838b2
Julia Kozak <jkozak30@stuy.edu>
2022-01-07 21:42:19 -0500

    driver

--- Driver.java
+++ Driver.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Driver {
 
 	public static ArrayList populate( int size, int lo, int hi ) {
@@ -8,7 +9,7 @@ public class Driver {
     		}
     		return retAL;
   	}
-  	
+
   	public static void shuffle( ArrayList al ) {
     		int randomIndex;
     		for( int i = al.size()-1; i > 0; i-- ) {
@@ -16,7 +17,113 @@ public class Driver {
       			al.set( i, al.set( randomIndex, al.get(i) ) );
     		}
   	}
-  	
-  	
-  
+
+		public static void reset( ArrayList<Comparable> w) {
+			for(int i=0; i<w.size(); i++) {
+				w.set(i, w.size()-i-1);
+			}
+		}
+
+		public static void main(String[] args) {
+
+			//best
+			System.out.println("---------- BEST CASES ----------");
+			ArrayList<Comparable> best = new ArrayList<Comparable>();
+			for (int i=0; i<10; i++) {
+				best.add(i);
+			}
+			Sorts.bubble(best);
+			Sorts.selection(best);
+			Sorts.insertion(best);
+			System.out.println();
+
+			for(int i=10; i<100; i++) {
+				best.add(i);
+			}
+			Sorts.bubble(best);
+			Sorts.selection(best);
+			Sorts.insertion(best);
+			System.out.println();
+
+			for(int i=100; i<1000; i++) {
+				best.add(i);
+			}
+			Sorts.bubble(best);
+			Sorts.selection(best);
+			Sorts.insertion(best);
+			System.out.println();
+
+			//worst
+			System.out.println("---------- WORST CASES ----------");
+			ArrayList<Comparable> worst = new ArrayList<Comparable>();
+			for(int i=0; i<10; i++) {
+				worst.add(0, i);
+			}
+			Sorts.bubble(worst);
+			reset(worst);
+			Sorts.selection(worst);
+			reset(worst);
+			Sorts.insertion(worst);
+			reset(worst);
+			System.out.println();
+
+			for(int i=10; i<100; i++) {
+				worst.add(0, i);
+			}
+			Sorts.bubble(worst);
+			reset(worst);
+			Sorts.selection(worst);
+			reset(worst);
+			Sorts.insertion(worst);
+			reset(worst);
+			System.out.println();
+
+			for(int i=100; i<1000; i++) {
+				worst.add(0, i);
+			}
+			Sorts.bubble(worst);
+			reset(worst);
+			Sorts.selection(worst);
+			reset(worst);
+			Sorts.insertion(worst);
+			reset(worst);
+			System.out.println();
+
+			//random 5 trials ea size
+			System.out.println("---------- RANDOM CASES ----------");
+			ArrayList<Comparable> r10 = new ArrayList<Comparable>();
+			for (int i=0; i<5; i++) {
+				r10 = populate(10, 0, 19);
+				Sorts.bubble(r10);
+				r10 = populate(10, 0, 19);
+				Sorts.selection(r10);
+				r10 = populate(10, 0, 19);
+				Sorts.insertion(r10);
+				System.out.println();
+			}
+
+			System.out.println("-----");
+			ArrayList<Comparable> r100 = new ArrayList<Comparable>();
+			for (int i=0; i<5; i++) {
+				r100 = populate(100, 0, 199);
+				Sorts.bubble(r100);
+				r100 = populate(100, 0, 199);
+				Sorts.selection(r100);
+				r100 = populate(100, 0, 199);
+				Sorts.insertion(r100);
+				System.out.println();
+			}
+
+			ArrayList<Comparable> r1000 = new ArrayList<Comparable>();
+			for (int i=0; i<5; i++) {
+				r1000 = populate(1000, 0, 1999);
+				Sorts.bubble(r1000);
+				r1000 = populate(1000, 0, 1999);
+				Sorts.selection(r1000);
+				r1000 = populate(1000, 0, 1999);
+				Sorts.insertion(r1000);
+				System.out.println();
+			}
+		}
+
 }
--- Sorts.java
+++ Sorts.java
@@ -2,28 +2,36 @@ import java.util.ArrayList;
 public class Sorts {
 
   public static void bubble( ArrayList<Comparable> data ) {
-    int bubblePassCtr = 0;
+    int passes = 0;
+    int swaps = 0;
+    int comps = 0;
     for(int i = 0; i < data.size()-1; i++) {
 
     	for(int j = data.size() - 1; j > i; j--){
+        comps++;
     		if((data.get(j)).compareTo(data.get(j-1)) < 0) {
           Comparable temp = data.get(j);
           data.set(j, data.get(j - 1));
           data.set(j-1, temp);
+          swaps++;
     		}
     	}
-    	bubblePassCtr++;
+    	passes++;
     }
+    System.out.println("Bubble: size " + data.size() + ", passes " + passes + ", swaps " + swaps + ", comparisons " + comps);
   }
 
   public static void selection( ArrayList<Comparable> data ) {
-    int selectionPassCtr = 0;
+    int passes = 0;
+    int swaps = 0;
+    int comps = 0;
     int minPos;
 
     for(int i=0; i<data.size()-1; i++) {
       minPos = i;
 
-      for(int j=i; j<data.size(); j++) {
+      for(int j=i+1; j<data.size(); j++) {
+        comps++;
         if ((data.get(j)).compareTo(data.get(minPos)) < 0) {
           minPos = j;
         }
@@ -31,27 +39,33 @@ public class Sorts {
       Comparable temp = data.get(i);
       data.set(i, data.get(minPos));
       data.set(minPos, temp);
+      swaps++;
+      passes++;
     }
-    selectionPassCtr++;
+    System.out.println("Selection: size " + data.size() + ", passes " + passes + ", swaps " + swaps + ", comparisons " + comps);
   }
 
   public static void insertion( ArrayList<Comparable> data ) {
-    int insertionPassCtr = 0;
+    int passes = 0;
+    int swaps = 0;
+    int comps = 0;
     for( int i=1; i<data.size(); i++ ) {
       for( int j=i; j>0; j-- ) {
 
+        comps++;
         if ((data.get(j)).compareTo(data.get(j-1)) < 0 ) {
           Comparable temp = data.get(j);
           data.set(j, data.get(j-1));
           data.set(j-1, temp);
+          swaps++;
         } else {
           break;
         }
 
       }
-      insertionPassCtr++;
+      passes++;
     }
-    
+    System.out.println("Insertion: size " + data.size() + ", passes " + passes + ", swaps " + swaps + ", comparisons " + comps);
   }
 
 }
